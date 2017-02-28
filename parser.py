128c57e612d97e5f1c053781f2c59104d0561afa
Quinn Stevens <quantumjump451@gmail.com>
2017-02-28 14:29:56 +0000

    Added main python file skeleton

---
+++ parser.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3.4
+
+# Import the various needed modules
+import sys
+
+if len(sys.argv) < 2:
+    # they didn't add a string to be parsed
+    if sys.argv[0] == 'parser.py':
+        print("Usage: python parser.py [dice notation]")
+        sys.exit()
+    else:
+        print("Usage: ./parser.py [dice notation]")
+        sys.exit()
+
+
