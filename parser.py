31993414d29f0bae5fce3655d4b338596199ed88
Quinn Stevens <quantumjump451@gmail.com>
2017-03-14 09:55:18 +0000

    moved parse() and roll() out of the main function

--- parser.py
+++ parser.py
@@ -8,6 +8,34 @@ import argparse
 import dice
 import dice_set
 
+def parse(toParse):
+    """
+    Parses a string for RPG dice notation, and also rolls this dice pool
+    """
+    parsedString = dicePat.search(toParse)
+    diceNo, sides = parsedString.groups()
+    print("Rolling " + diceNo + ' dice with ' + sides + ' sides each.')
+    # print("Result: " + str(roll(diceNo, sides)))
+
+    sides = int(sides)
+    diceNo = int(diceNo)
+    
+    diceList = [dice.Dice(sides) for i in range(diceNo)]
+    
+    dicePool = dice_set.DiceSet(diceList)
+    result = dicePool.roll()
+    print('{sum}: {result}'.format(sum=sum(result), result=result))
+
+
+def roll(number, sides):
+    """
+    Rolls number dice, each with sides sides, returns the sum of them all
+    """
+    total = 0
+    for i in range(int(number)):
+        result = random.randint(1, int(sides))
+        total += result
+    return total
 
 
 def main():
@@ -18,39 +46,7 @@ def main():
     args = parser.parse_args()
 
     inputString = ' '.join(args.notation)
-    dicePat = re.compile(r'(\d+)d(\d+)')
-
-
-    def roll(number, sides):
-        """
-        Rolls number dice, each with sides sides, returns the sum of them all
-        """
-        total = 0
-        for i in range(int(number)):
-            result = random.randint(1, int(sides))
-            total += result
-        return total
-    
-    
-    def parse(toParse):
-        """
-        Parses a string for RPG dice notation, and also rolls this dice pool
-        """
-        parsedString = dicePat.search(toParse)
-        diceNo, sides = parsedString.groups()
-        print("Rolling " + diceNo + ' dice with ' + sides + ' sides each.')
-        # print("Result: " + str(roll(diceNo, sides)))
-    
-        sides = int(sides)
-        diceNo = int(diceNo)
-    
-        diceList = [dice.Dice(sides) for i in range(diceNo)]
-    
-        dicePool = dice_set.DiceSet(diceList)
-        result = dicePool.roll()
-        print('{sum}: {result}'.format(sum=sum(result), result=result))
-    
-    
+    dicePat = re.compile(r'(\d+)d|D(\d+)')    
     
     parse(inputString)
 
